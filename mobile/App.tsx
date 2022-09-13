46f0c0becf5c6ce0954a9bfe6d5da94342f03038
h3nrey <pedro.novaes.education@gmail.com>
2022-09-13 18:37:09 -0300

    criando a primeira tela do mobile (aula 2)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StatusBar, View, Text} from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter";
 
-interface ButtonProps{
-  title: string,
-}
+import { Home } from "./src/screens/Home";
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
 
-function Button(props:ButtonProps) {
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+
 
 export default function App() {
+  const [fontsLoaded]= useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Button title="Send"/>
+    <Background>     
+      <Text>teste</Text>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      /> 
+      
+      <View>
+      </View>
 
-    </View>
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
