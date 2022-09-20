990b13f4f3b97ce0c014d57125053e957603e812
h3nrey <pedro.novaes.education@gmail.com>
2022-09-20 14:59:14 -0300

    conectando com a rota que busca com todos os anúncios

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,13 +6,14 @@ import './styles/main.css';
 import logoImg from "./assets/logo-nlw-esports.svg"
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
-import { GameController } from 'phosphor-react';
+import { GameController, MagnifyingGlassPlus } from 'phosphor-react';
 import { Input } from './components/Form/input';
 import { CreateAdModal } from './components/CreateAdModal';
 
 import { GamesCarousel } from './components/GamesCarousel';
+import { LastAds } from './components/LastAds';
 
-interface Game{
+export interface Game{
   id: string;
   title: string;
   bannerUrl: string;
@@ -21,27 +22,52 @@ interface Game{
   }
 }
 
+export interface Ad{
+  id: string,
+	gameId: string,
+	name: string,
+	yearsPlaying: number,
+	discord: string,
+	weekDays: "0,5,6",
+	hourStart: string,
+	hourEnd: string,
+	useVoiceChanel: boolean,
+}
+
 function App() {
   const [games, setGames] = useState<Game[]>([])
+  const [ads, setAds] = useState<Ad[]>([])
 
   useEffect(() => {
     axios('http://localhost:8000/games').then(res => {
       setGames(res.data);
     })
+    axios('http://localhost:8000/games/ads').then(res => {
+      setAds(res.data);
+    })
   }, [])
 
+  
+
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="logo" />
-      <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
+    <div className="mx-auto flex flex-col items-center my-20 px-8 lg:px-20">
+      <div className='flex flex-col items-center md:flex-row md:items-end gap-8 w-full'>
+        <img src={logoImg} alt="logo" />
+        <h1 className='text-4xl text-white font-black text-center md:text-start'>Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
+      </div>
 
-      <GamesCarousel items={games}/>
+      <section className='flex flex-col w-full mt-20 gap-8'>
+        <h2 className='font-semibold text-3xl text-white'>Jogos mais populares</h2>
+        <GamesCarousel items={games}/>
+      </section>
 
+      <LastAds props={{games,ads}}/>
       <Dialog.Root >
         <CreateAdBanner />
 
         <CreateAdModal />
       </Dialog.Root>
+      
     </div>
   )
 }
